d7bb3e2621665deb1fa82af6aa215153ab515911
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2019-10-27 09:37:59 -0500

    Finish the auth form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -3,6 +3,7 @@ import { LoadingController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
@@ -10,6 +11,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -37,5 +39,24 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup server
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
